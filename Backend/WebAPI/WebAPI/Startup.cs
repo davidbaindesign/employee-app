9a856b91d9212f8f8676cb3e61c9ee502689dba3
davidbaindesign <davidbaindesign@gmail.com>
2021-09-06 17:31:35 -0400

    enabled cors

--- Backend/WebAPI/WebAPI/Startup.cs
+++ Backend/WebAPI/WebAPI/Startup.cs
@@ -22,12 +22,20 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //enable coors
+            services.AddCors(c =>
+            {
+                c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            });
+
             services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
