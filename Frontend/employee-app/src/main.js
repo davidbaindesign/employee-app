38e3b78b8065eba2767b0281f2327d25c3a6a251
davidbaindesign <davidbaindesign@gmail.com>
2021-09-15 22:12:01 -0400

    api test

--- Frontend/employee-app/src/main.js
+++ Frontend/employee-app/src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+fetch("http://localhost:5000/api/department")
+.then(response => response.json())
+.then(data => {console.log(data)
+});
+
+
 
 new Vue({
   render: h => h(App),
